d9adbf64799902ca3bd6f44595ce7ac667dfba63
Nicolas Ochsner <ochsnern@student.ethz.ch>
2021-04-07 09:46:34 +0200

    Slim binary variable addition.

--- misolib/load.py
+++ misolib/load.py
@@ -2,6 +2,9 @@
 from reframed.solvers.solver import VarType
 
 
+BOUND_INF = 1000
+
+
 def introduce_binary_variables(community, solver, min_growth=0.01):
     """Add binary variables to the solver instance.
 
@@ -15,43 +18,26 @@ def introduce_binary_variables(community, solver, min_growth=0.01):
 
     solver.update()
 
-    bound = 1000
-
     for org_id, org_model in community.organisms.items():
         org_var = f"y_{org_id}"
         for r_id in org_model.reactions.keys():
             merged_id = community.reaction_map[(org_id, r_id)]
-
-            if r_id == org_model.biomass_reaction:
-                solver.add_constraint(
-                    f"c_{merged_id}_lb",
-                    {merged_id: 1, org_var: -min_growth},
-                    ">",
-                    0,
-                    update=False,
-                )
-                solver.add_constraint(
-                    f"c_{merged_id}_ub",
-                    {merged_id: 1, org_var: -bound},
-                    "<",
-                    0,
-                    update=False,
-                )
-            else:
-                solver.add_constraint(
-                    f"c_{merged_id}_lb",
-                    {merged_id: 1, org_var: bound},
-                    ">",
-                    0,
-                    update=False,
-                )
-                solver.add_constraint(
-                    f"c_{merged_id}_ub",
-                    {merged_id: 1, org_var: -bound},
-                    "<",
-                    0,
-                    update=False,
-                )
+            lbound = -min_growth if r_id == org_model.biomass_reaction else BOUND_INF
+            ubound = BOUND_INF
+            solver.add_constraint(
+                f"c_{merged_id}_lb",
+                {merged_id: 1, org_var: lbound},
+                ">",
+                0,
+                update=False,
+            )
+            solver.add_constraint(
+                f"c_{merged_id}_ub",
+                {merged_id: 1, org_var: ubound},
+                "<",
+                0,
+                update=False,
+            )
 
 
 def setup_medium(model, solver, base_medium, carbon_sources):
