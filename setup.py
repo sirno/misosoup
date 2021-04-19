e4f08e953fe469d66c8a5931f9a8ae2d7ff0410a
Nicolas Ochsner <ochsnern@student.ethz.ch>
2021-04-19 14:25:34 +0200

    Bump version.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages
 setuptools.setup(
     name="misosoup",
     packages=find_packages(),
-    version="0.1.0",
+    version="0.2.0",
     author="Nicolas Ochsner",
     author_email="ochsnern@student.ethz.ch",
     scripts=["scripts/misosoup", "scripts/taste_soup", "scripts/filter_soup"],
